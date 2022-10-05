00dcc54df68ca67c770b658aecfb64d77cd43400
bbjarvis <brettbjarvis@gmail.com>
2022-10-05 11:32:40 -0700

    add models folder and mongodb schemas, remove dummy data from schema.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://bbjarvis:test123@gql-ninja.wn63lik.mongodb.net/test')
+mongoose.connection.once('open', ()=> {
+    console.log('connected to database')
+})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
