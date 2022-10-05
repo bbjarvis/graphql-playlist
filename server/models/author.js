da8945b60846b1e645fc02c1164c962ae0325dc8
bbjarvis <brettbjarvis@gmail.com>
2022-10-05 11:58:04 -0700

    add mutation for author

--- server/models/author.js
+++ server/models/author.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const authorSchema = new Schema({
     name: String,
     age: Number,
 })
 
-module.exports = mongoose.model('Book', bookSchema);
+module.exports = mongoose.model('Author', authorSchema);
