a7f7dd044c82074aef11b786a3c60644f4a2ecd2
bbjarvis <brettbjarvis@gmail.com>
2022-10-17 09:00:54 -0700

    fix bug in schema AthorType return statement

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,6 +12,20 @@ const {
     GraphQLList
 } = graphql;
 
+// var books = [
+//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+//     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+//     { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+//     { name: "The Long Earth", genre: "Sci-Fi", id: '3', authorId: '3' },
+//     { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+//     { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+// ];
+
+// var authors = [
+//     { name: 'Patrick Rothfuss', age: 44, "id": "63470897345bafbe7f60e3c7" },1
+//     { name: 'Brandon Sanderson', age: 42, "id": "634708cf345bafbe7f60e3c9" },2
+//     { name: 'Terry Pratchett', age: 66, "id": "6345c5f9e834a237fa744a2e"}3
+// ];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -39,7 +53,7 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 // return _.filter(books, { authorId: parent.id });
-                return Book.findById({authorId: parent.id});
+                return Book.find({authorId: parent.id});
 
             }
         }
