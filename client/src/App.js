e98fc59b43199849c291df27da932c24cd0e4924
bbjarvis <brettbjarvis@gmail.com>
2022-10-18 10:58:06 -0700

    add apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 // components
 import BookList from "./components/BookList";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+   cache: new InMemoryCache( ),
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Brett's Reading List</h1>
-      <BookList />
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Brett's Reading List</h1>
+        <BookList />
 
-    </div>
+      </div>
+      </ApolloProvider>
   );
 }
 
