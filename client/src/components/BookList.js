3e7ae8268e20e1b071a7f3a30a50f40f38fb6578
bbjarvis <brettbjarvis@gmail.com>
2022-10-26 16:28:12 -0700

    add pulling data from server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -16,14 +16,18 @@ const getBooksQuery = gql`
 function BookList() {
   const { loading, error, data } = useQuery(getBooksQuery);
   console.log(data);
+  if (loading) return <p>Loading....</p>
+  if (error) return <p>Something went wrong</p>
+  return data.books.map(book => {
 
     return (
       <div>
         <ul id="book-list">
-            <li>Book name</li>
+            <li key={book.id}>{book.name}</li>
         </ul>
       </div>
     );
-  }
+  })
+}
   
   export default BookList;
